f1163487032f4325184596f6bae88f48e663eba5
Brett Davidson <brett_davidson@yahoo.com>
2019-06-19 14:23:17 -0400

    Convert Celcius to Fahrenheit

--- weather.py
+++ weather.py
@@ -1,14 +1,22 @@
 from datetime import datetime
+from dotenv import load_dotenv
 import os
 import pytz
 import requests
 import math
-API_KEY = 'd8286b706924566b097731278611da9f'
+
+load_dotenv()  # obtain the weather key from .env file
+
+API_KEY = os.getenv("WEATHER_KEY")
+
 API_URL = ('http://api.openweathermap.org/data/2.5/weather?q={}&mode=json&units=metric&appid={}')
 def query_api(city):
     try:
         print(API_URL.format(city, API_KEY))
         data = requests.get(API_URL.format(city + ",US", API_KEY)).json()
+        print(data['main']['temp'])
+        data['main']['temp'] = data['main']['temp'] * 9.0/5.0 + 32  # Convert celcius to fahrenheit
+        print(data['main']['temp'])
     except Exception as exc:
         print(exc)
         data = None
